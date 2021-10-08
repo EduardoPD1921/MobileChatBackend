72b4c2d61fdc474b7c1b670e6da0f9ec421e4d81
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2021-10-08 01:01:19 -0300

    :wrench: Setting default route to get method

--- src/api/app.js
+++ src/api/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: '*' }));
 
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.status(200).send({ message: 'API v1.0' });
 });
 app.use('/user', userRoutes);
