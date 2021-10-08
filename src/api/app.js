6bbc8a539b978f2b2bfc1da92c81d609e4fbc300
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2021-10-08 00:54:10 -0300

    :sparkles: Default route returning api version

--- src/api/app.js
+++ src/api/app.js
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: '*' }));
 
+app.use('/', (req, res, next) => {
+  res.status(200).send({ message: 'API v1.0' });
+});
 app.use('/user', userRoutes);
 
 module.exports = app;
