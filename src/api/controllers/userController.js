9fe4d21c97759a3ceb7f8f325dd575a387b0af2b
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2021-11-17 00:08:55 -0300

    :wrench: mooooooooore config

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -15,7 +15,7 @@ exports.store = async (req, res, _next) => {
     await user.save();
     res.status(201).send('user-created');
   } catch (error) {
-    res.status(400).send({ message: error });
+    res.status(400).send({ message: error.message });
   }
 }; 
 
